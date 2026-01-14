06660995f6542f6262c3890c22b6b45ab4ddf7a0
Toby Donaldson <tjd@sfu.ca>
2026-01-14 08:55:14 -0800

    updated chp2 notes

--- lecture_notes/chapter1/demo.py
+++ lecture_notes/chapter1/demo.py
@@ -1,1 +1,13 @@
-print('Hello, world!')
+# demo.py
+
+#print('Hello, world!')
+#print('Hello ' + 'world' + '!')
+#print('Hello ' + 'world' + '!'*3)
+
+print(7 / 2)
+print(7 // 2)
+print(1/2 + 1/2)
+print(1//2 + 1//2)
+
+print(2 ** 10)
+print(3.4 ** 2.2)
