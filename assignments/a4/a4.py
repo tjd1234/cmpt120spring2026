0d34c2a2d6c0fe5af99de1c1c9aaa35ce797e633
Toby Donaldson <tjd@sfu.ca>
2026-02-25 22:01:36 -0800

    updated asn 4

---
+++ assignments/a4/a4.py
@@ -0,0 +1,9 @@
+# a4.py
+
+#
+# Full Name:
+#  SFU ID #:
+# SFU Email:
+#
+
+# ... your code goes here ...
---
+++ lecture_notes/chapter7/m_words.py
@@ -0,0 +1,22 @@
+# m_word_pct.py
+
+def is_m_word(word):
+    for c in word:
+        if c in 'mM':
+            return True
+    return False
+
+def check_if_m_word(word):
+    if is_m_word(word):
+        print(f'"{word}" has an m')
+    else:
+        print(f'"{word}" does not have an m')
+
+check_if_m_word('mumble')
+check_if_m_word('apple')
+check_if_m_word('')
+
+def jump_to(x, y):
+    turtle.penup()
+    turtle.goto(x, y)
+    turtle.pendown()
