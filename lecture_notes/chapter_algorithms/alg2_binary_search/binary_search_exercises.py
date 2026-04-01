f4a9e3eba85a6256da2223ad7253a2188c03a27c
Toby Donaldson <tjd@sfu.ca>
2026-04-01 11:23:56 -0700

    updated algorithm notes

--- lecture_notes/chapter_algorithms/alg2_binary_search/binary_search_exercises.py
+++ lecture_notes/chapter_algorithms/alg2_binary_search/binary_search_exercises.py
@@ -26,11 +26,16 @@
 # TODO 1 Create a new variable called names initialized to this sorted list of
 # strings: "Alice", "Bob", "Charlie", "Diana", "Eve". Print it to make sure it
 # is correct.
+names = ["Alice", "Bob", "Charlie", "Diana", "Eve"]
+print(names)
 
 # TODO 2 Print the length of names using the len() function.
+print(len(names))
 
 # TODO 3 Print the first and last names, and also print the the list in reverse
 # alphabetical order.
+print(names[0], names[-1], names[::-1])
+print(names)
 
 
 #
@@ -39,11 +44,15 @@
 
 # TODO 4 Binary search works by checking the middle element of a list. What are
 # the middle elements of names? What is the middle element of scores?
+names = ["Alice", "Bob", "Charlie", "Diana", "Eve", "Fred"]
+print(names[2])
 
 # TODO 5 Suppose L is a list of length n, with its elements in sorted order.
 # What is the general formula, using n, for the middle element of L? Use this
-# formula to print the middle element of names and scores. Hint: Recall that a
-# // b is integer division.
+# formula to print the middle element of names and scores. Hint: Recall that 
+# a // b is integer division.
+
+# len(L) // 2   (if there's a .5, it's chopped off, e.g. 5 // 2 = 2)
 
 
 # TODO 6 After binary search finds the middle element, it checks to see if the
@@ -53,7 +62,16 @@
 # print whether it is equal to it, less than it, or greater than it.
 #
 # Test it with targets "Charlie", "Allie", and "Zara".
-
+target = "Zara"
+names = ["Alice", "Bob", "Charlie", "Diana", "Eve", "Fred"]
+n = len(names)
+mid = n // 2
+if target == names[mid]:
+    print(f'Found it at {mid}')
+elif target < names[mid]:
+    print(f'{target} might be in left half')
+else: # target > names[mid]
+    print(f'{target} might be in right half')
 
 # TODO 7 Re-do the previous task, but this time change elif and else parts to
 # print the slice of the list where the target might be. Specifically:
@@ -63,7 +81,18 @@
 #   mid + 1 to to the end
 # 
 # Test it with targets "Charlie", "Allie", and "Zara".
-
+target = "Zara"
+names = ["Alice", "Bob", "Charlie", "Diana", "Eve", "Fred"]
+n = len(names)
+mid = n // 2
+if target == names[mid]:
+    print(f'Found {target} at {mid}')
+elif target < names[mid]:
+    left = names[:mid]
+    print(f'{target} might be in left half: {left}')
+else: # target > names[mid]
+    right = names[mid+1:]
+    print(f'{target} might be in right half: {right}')
 
 #
 # a binary search function
@@ -78,18 +107,63 @@
 #
 # For now, have the function just print "Searching for <target> ..." and return
 # False.
-
+def binary_search(lst, target):
+    print(f'binary search for {target} in {lst} ...')
+    return False
 
 # TODO 9 Now replace the body of the function with the if-elif-else statement
 # from above (the one with the slices). Make sure to include mid as a local
 # variable at the start. Test it with the list of names and a few different
 # targets.
+# def binary_search(lst, target):
+#     n = len(lst)
+#     mid = n // 2
+#     if target == lst[mid]:
+#         print(f'Found {target} at {mid}')
+#         return True
+#     elif target < lst[mid]:
+#         left = lst[:mid]
+#         print(f'{target} might be in left half: {left}')
+#         return False # todo: fix this
+#     else: # target > lst[mid]
+#         right = lst[mid+1:]
+#         print(f'{target} might be in right half: {right}')
+#         return False # todo: fix this
 
+# names = ["Alice", "Bob", "Charlie", "Diana", "Eve", "Fred"]
+# print(binary_search(names, "Diana"))
+# print(binary_search(names, "Zara"))
+# print('empty list', binary_search([], "Zara"))
 
 # TODO 10 We need to handle a special case: when lst is empty, binary search
 # should immediately return False. Re-write the function from the previous task
 # so that it returns False when lst is empty (and keep everything else the
 # same). Test it with the list [] and target "Charlie".
+
+def binary_search(lst, target):
+    if lst == []: # base case for recursion
+        return False # target not in list
+
+    n = len(lst)
+    mid = n // 2
+    if target == lst[mid]:
+        # print(f'Found {target} at {mid}')
+        return True
+    elif target < lst[mid]:
+        left = lst[:mid]
+        # print(f'{target} might be in left half: {left}')
+        return binary_search(left, target)
+    else: # target > lst[mid]
+        right = lst[mid+1:]
+        # print(f'{target} might be in right half: {right}')
+        return binary_search(right, target)
+
+names = ["Jane", "Mike", "Zeke", "Tina", "Alice", "Bob", "Charlie", "Diana", "Eve", "Fred"]
+names.sort()
+print(names)
+print(binary_search(names, "Diana")) # True
+print(binary_search(names, "Zara")) # False
+print('empty list', binary_search([], "Zara")) # False
 
 
 # TODO 11 Copy the function in the previous task and modify it so that it turns
